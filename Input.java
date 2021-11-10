a399d7951ab6f8d17e37a59dc68681fce42dc4b7
Irfan Farooq <irfandominate@gmail.com>
2021-11-10 19:45:30 +0530

    first commit

---
+++ Input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Input {
+    public  static void main(String[] args) {
+    
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+        System.out.println(input.next());
+        // System.out.println(input.nextLine());
+        input.close();
+       }
+}
+
---
+++ primtive.java
@@ -0,0 +1,10 @@
+public class primtive {
+   public static void main(String[] args) {
+       int rollno=64;
+       char letter='r';
+       float marks=98.67f;
+       double largeDecimalNumbers=593774.734774;
+       long largeInteger=9859530968967309L;
+       boolean check =true;
+   }
+}
