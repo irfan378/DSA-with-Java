722ee0b824339c5cd8d0b5265538a124e202704f
Irfan Farooq <irfandominate@gmail.com>
2021-11-20 20:32:40 +0530

    added shadowing function overloading

---
+++ armstrongNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class armstrongNumber {
+   public static void main(String[] args) {
+       Scanner in=new Scanner(System.in);
+       int n=in.nextInt();
+       System.out.println(isArmstrong(n));
+   }
+   static boolean isArmstrong(int n){
+       int original=n;
+       int sum=0;
+       while(n>0){
+           int rem=n%10;
+           n=n/10;
+           sum=sum+rem*rem*rem;
+       }
+       if(sum==original){
+           return true;
+       }
+       return false;
+   }
+}
