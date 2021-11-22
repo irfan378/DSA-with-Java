711e5a267bb8595d278d0cc5d8bffc37a2a6ce5c
Irfan Farooq <irfandominate@gmail.com>
2021-11-22 20:56:57 +0530

    added multidimensional arrays

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,3 +1,5 @@
- // for (int i = 0; i < arr.length; i++) {
-        //     System.out.print(arr[i] + " ");
+  // int[]arr=new int[5];
+        // // input using for loops
+        // for (int i = 0; i < arr.length; i++) {
+        //     arr[i]=in.nextInt();
         // }
