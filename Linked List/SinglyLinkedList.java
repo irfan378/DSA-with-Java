09aca3aa50646c4e796d47a1af0204902023bf59
Irfan Farooq <irfandominate@gmail.com>
2022-03-16 18:02:32 +0530

    insert at first or last postion in linkedlist

--- Linked List/SinglyLinkedList.java
+++ Linked List/SinglyLinkedList.java
@@ -10,6 +10,39 @@ public class SinglyLinkedList {
             this.size = 0;
         }
 
+        public void insertFirst(int val) {
+            Node node = new Node(val);
+            node.next = head;
+            head = node;
+
+            if (tail == null) {
+                tail = head;
+            }
+            size += 1;
+        }
+
+        public void insertLast(int val) {
+            if (tail == null) {
+                insertFirst(val);
+                return;
+            }
+
+            Node node = new Node(val);
+            tail.next = node;
+            tail = node;
+            size++;
+
+        }
+
+        public void display() {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.value + " -> ");
+                temp = temp.next;
+            }
+            System.out.println("END");
+        }
+
         private class Node {
             private int value;
             private Node next;
@@ -23,11 +56,17 @@ public class SinglyLinkedList {
                 this.next = next;
             }
         }
+
     }
 
     public static void main(String[] args) {
         LL list = new LL();
-
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(10);
+        list.insertLast(99);
+        list.display();
     }
 
 }
