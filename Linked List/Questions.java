b50e58f1604d4c2a2a4f147f30ecf34a223c28da
Irfan Farooq <irfandominate@gmail.com>
2022-03-23 17:38:27 +0530

    added a question on linked list

--- Linked List/Questions.java
+++ Linked List/Questions.java
@@ -1,6 +1,4 @@
-import java.util.LinkedList;
-
-public class removeDuplicateFromSortedList {
+public class Questions {
 
     public static class LL {
         private Node head;
@@ -160,20 +158,76 @@ public class removeDuplicateFromSortedList {
             tail.next = null;
         }
 
+        // merge
+        public static LL merge(LL first, LL second) {
+            Node f = first.head;
+            Node s = second.head;
+
+            LL ans = new LL();
+            while (f != null && s != null) {
+                if (f.value < s.value) {
+                    ans.insertLast(f.value);
+                    f = f.next;
+                } else {
+                    ans.insertLast(s.value);
+                    s = s.next;
+                }
+            }
+            while (f != null) {
+                ans.insertLast(f.value);
+                f = f.next;
+            }
+            while (s != null) {
+                ans.insertLast(s.value);
+                s = s.next;
+            }
+            return ans;
+
+        }
+
+        // has cycle
+        public boolean hasCycle() {
+            Node fast = head;
+            Node slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 
     public static void main(String[] args) {
-        LL list = new LL();
-        list.insertLast(1);
-        list.insertLast(1);
-        list.insertLast(2);
-        list.insertLast(2);
-        list.insertLast(2);
-        list.insertLast(3);
-        list.insertLast(4);
-        list.display();
-        list.duplicates();
-        list.display();
+        // LL list = new LL();
+        // list.insertLast(1);
+        // list.insertLast(1);
+        // list.insertLast(2);
+        // list.insertLast(2);
+        // list.insertLast(2);
+        // list.insertLast(3);
+        // list.insertLast(4);
+
+        // list.duplicates();
+        // list.display();
+
+        // LL first = new LL();
+        // LL second = new LL();
+
+        // first.insertLast(1);
+        // first.insertLast(3);
+        // first.insertLast(5);
+
+        // second.insertLast(1);
+        // second.insertLast(2);
+        // second.insertLast(9);
+        // second.insertLast(14);
+
+        // LL ans = LL.merge(first, second);
+        // ans.display();
 
     }
 
