8e3e7155eb39d62ebfda2c2a0794f3c2b6f93e94
Irfan Farooq <irfandominate@gmail.com>
2022-03-24 21:34:12 +0530

    added questionn on linked list

--- Linked List/Questions.java
+++ Linked List/Questions.java
@@ -1,3 +1,5 @@
+import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag;
+
 public class Questions {
 
     public static class LL {
@@ -197,6 +199,58 @@ public class Questions {
                 }
             }
             return false;
+        }
+
+        // Length of cycle
+        public int lengthOfCycle(Node head) {
+            Node fast = head;
+            Node slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    // calculate the length
+                    Node temp = slow;
+                    int length = 0;
+                    do {
+                        temp = temp.next;
+                        length++;
+                    } while (temp != slow);
+                    return length;
+                }
+            }
+            return 0;
+        }
+
+        // detect Cycle
+        public Node detectCycle(Node head) {
+            int length = 0;
+            Node fast = head;
+            Node slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    length = lengthOfCycle(slow);
+                    break;
+                }
+            }
+            if (length == 0) {
+                return null;
+            }
+            // find the start node
+            Node f = head;
+            Node s = head;
+            while (length > 0) {
+                s = s.next;
+                length--;
+            }
+            // Keep moving both forward and they will meet at cycle start.
+            while (f != s) {
+                f = f.next;
+                s = s.next;
+            }
+            return s;
         }
 
     }
