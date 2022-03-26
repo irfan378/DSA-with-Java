eef3bed349bd068f1bc9081615389b820aa57431
Irfan Farooq <irfandominate@gmail.com>
2022-03-26 21:00:50 +0530

    added question on linked list

--- Linked List/Questions.java
+++ Linked List/Questions.java
@@ -1,5 +1,3 @@
-import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag;
-
 public class Questions {
 
     public static class LL {
@@ -286,6 +284,41 @@ public class Questions {
                 slow = slow.next;
             }
             return slow;
+        }
+
+        public void bubbleSort() {
+            bubbleSort(size - 1, 0);
+        }
+
+        private void bubbleSort(int row, int col) {
+            if (row == 0) {
+                return;
+            }
+            if (col < row) {
+                Node first = get(col);
+                Node second = get(col + 1);
+
+                if (first.value > second.value) {
+                    // swap
+                    head = second;
+                    first.next = second.next;
+                    second.next = first;
+                } else if (second == tail) {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    tail = first;
+                    first.next = null;
+                    second.next = tail;
+                } else {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    first.next = second.next;
+                    second.next = first;
+                }
+                bubbleSort(row, col + 1);
+            } else {
+                bubbleSort(row - 1, 0);
+            }
         }
 
     }
