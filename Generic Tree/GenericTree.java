49814e96853a39aaae62e008635b6741aca32fe5
Irfan Farooq <irfandominate@gmail.com>
2022-05-08 15:22:32 +0530

    questions on generic tree

--- Generic Tree/GenericTree.java
+++ Generic Tree/GenericTree.java
@@ -7,6 +7,14 @@ public class GenericTree {
     private static class Node {
         int data;
         ArrayList<Node> children = new ArrayList<>();
+
+        Node() {
+
+        }
+
+        Node(int data) {
+            this.data = data;
+        }
     }
 
     public static void display(Node node) {
@@ -120,6 +128,70 @@ public class GenericTree {
                 cs = new Stack<>();
                 level++;
                 System.out.println();
+            }
+        }
+    }
+
+    public static void levelOrderLineWise2(Node node) {
+        Queue<Node> mq = new ArrayDeque<>();
+        mq.add(node);
+        mq.add(new Node(-1));
+        while (mq.size() > 0) {
+            node = mq.remove();
+            if (node.data != -1) {
+                System.out.print(node.data + " ");
+                for (Node child : node.children) {
+                    mq.add(child);
+                }
+            } else {
+                if (mq.size() > 0) {
+                    mq.add(new Node(-1));
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public static void levelOrderLineWise3(Node node) {
+        Queue<Node> mq = new ArrayDeque<>();
+        mq.add(node);
+        while (mq.size() > 0) {
+            int cicl = mq.size();
+            for (int i = 0; i < cicl; i++) {
+                node = mq.remove();
+                System.out.print(node.data + " ");
+                for (Node child : node.children) {
+                    mq.add(child);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static class Pair {
+        Node node;
+        int level;
+
+        Pair(Node node, int level) {
+            this.node = node;
+            this.level = level;
+        }
+    }
+
+    public static void levelOrderLineWise4(Node node) {
+        Queue<Pair> mq = new ArrayDeque<>();
+        mq.add(new Pair(node, 1));
+        int level = 1;
+        while (mq.size() > 0) {
+            Pair p = mq.remove();
+            if (p.level > level) {
+                level = p.level;
+                System.out.println();
+            }
+            System.out.println(p.node.data + " ");
+            for (Node child : node.children) {
+                Pair cp = new Pair(child, p.level + 1);
+                mq.add(cp);
             }
         }
     }
