0f826be60e6cb0ba3c0442a1f00cb1fc9a40b229
irfan378 <irfandominate@gmail.com>
2022-08-19 18:43:23 +0530

    question on hashmap

---
+++ Hashmap and Heaps/GetCommonElement2.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class GetCommonElement2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        int[] a1 = new int[n1];
+        for (int i = 0; i < a1.length; i++) {
+            a1[i] = scn.nextInt();
+        }
+
+        int n2 = scn.nextInt();
+        int[] a2 = new int[n2];
+        for (int i = 0; i < a2.length; i++) {
+            a2[i] = scn.nextInt();
+        }
+        HashMap<Integer, Integer> fmap = new HashMap<>();
+        for (int val : a1) {
+            if (fmap.containsKey(val)) {
+                int oldFreq = fmap.get(val);
+                int newFreq = oldFreq + 1;
+                fmap.put(val, newFreq);
+            } else {
+                fmap.put(val, 1);
+            }
+        }
+        for (int val : a2) {
+            if (fmap.containsKey(val) && fmap.get(val) > 0) {
+                System.out.println(val);
+                int oldFreq = fmap.get(val);
+                int newFreq = oldFreq - 1;
+                fmap.put(val, newFreq);
+            }
+        }
+    }
+}
